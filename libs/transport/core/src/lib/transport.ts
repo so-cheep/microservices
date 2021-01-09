996df4a145adbeaa7d49ba8b6a422807c6705546
playerx <ez@jok.io>
2021-01-10 01:09:47 +0400

    docs(transport): add docs information on transport interface

--- libs/transport/core/src/lib/transport.ts
+++ libs/transport/core/src/lib/transport.ts
@@ -1,51 +1,43 @@
 export interface Transport {
   /**
-   * - rabbitmq         - publish new message to the queue
-   * - socket.io-server - send message to the client, based on the socketId in metadata
-   * - socket.io-client - send message to the server
-   */
-  publish(props: PublishProps<MessageMetadata>): Promise<void>
-
-  execute(props: ExecuteProps<MessageMetadata>): Promise<unknown>
-
-  /**
-   * - rabbitmq         - create binding (exchange -> queue)
-   * - socket.io-server - do nothing
-   * - socket.io-client - adds event to listen (?)
-   */
-  // listenPatterns(patterns: string[]): Promise<void>
-
-  /**
    * Make sure all entities are initialized
    */
   init(): Promise<void>
-
-  /**
-   * rabbitmq         - start connection
-   * socket.io-server - start server listening process
-   * socket.io-client - connect to the server
-   */
-  start(): Promise<void>
-
-  /**
-   * rabbitmq         - stop connection
-   * socket.io-server - stop listening connections
-   * socket.io-client - disconnect from the server
-   */
-  stop(): Promise<void>
-
-  /**
-   * rabbitmq         - stop and clear resources
-   * socket.io-server - stop and clear resources
-   * socket.io-client - stop and clear resources
-   */
-  dispose(): Promise<void>
 
   on(route: string, action: RouteHandler): void
 
   off(route: string): void
 
-  onEvery(prefixes: string[], action: FireAndForgetHandler)
+  onEvery(prefixes: string[], action: FireAndForgetHandler): void
+
+  /**
+   * At this point all handlers are registered and we can
+   * configure Exchange->Queue bindings
+   */
+  start(): Promise<void>
+
+  /**
+   * Publish event, no result will be returned
+   */
+  publish(props: PublishProps<MessageMetadata>): Promise<void>
+
+  /**
+   * Execute RPC call, result will be returned always
+   *
+   * throws RPCTimeout error
+   */
+  execute(props: ExecuteProps<MessageMetadata>): Promise<unknown>
+
+  /**
+   * Stop connection to the queues
+   */
+  stop(): Promise<void>
+
+  /**
+   * Make sure all resources are disposed
+   * Temp queues should be deleted
+   */
+  dispose(): Promise<void>
 }
 
 export interface TransportMessage {
