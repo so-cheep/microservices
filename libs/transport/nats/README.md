8dcac0c2055c5470e7e9571f87ba8d82d275375b
Kyle Bjordahl <3489222+kylebjordahl@users.noreply.github.com>
2021-02-01 19:26:42 -0800

    feat(transport-nats): naive nats transport

---
+++ libs/transport/nats/README.md
@@ -0,0 +1,9 @@
+# @cheep/nats
+
+A [nats](https://nats.io/) transport for the Cheep infrastructure
+
+> This is built with the `nats@beta` version of the JS library, so use it with a grain of salt until they release v2
+
+## Running unit tests
+
+Run `nx test transport-nats` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/transport/nats/src/lib/nats.transport.ts
@@ -0,0 +1,157 @@
+import {
+  MessageMetadata,
+  SendMessageProps,
+  SendReplyMessageProps,
+  TransportBase,
+  TransportOptions,
+  TransportUtils,
+} from '@cheep/transport'
+import { connect, NatsConnection, Subscription, headers } from 'nats'
+
+// TODO: support chunking large messages over the wire
+// TODO: support NKey authentication
+// TODO: support Certificat authentication
+// TODO: implement dead letter queue
+
+export class NatsTransport<
+  TMeta extends MessageMetadata = MessageMetadata
+> extends TransportBase {
+  private nc: NatsConnection
+  private subscriptions: Subscription[]
+  private correlationIdHeader = 'X-CHEEP-CORRELATION-ID'
+
+  constructor(
+    protected options: TransportOptions<TMeta> & {
+      moduleName: string
+      /** array of urls to connect to, may optionally contain username and password or token encoded in the url */
+      natsServerUrls: string[]
+      isTestMode?: boolean
+      maxPingOut?: number
+
+      /** optional authentication method with password */
+      user?: string
+      /** optional authentication method with user */
+      password?: string
+
+      /** optional authentication method */
+      token?: string
+    },
+    protected utils: TransportUtils,
+  ) {
+    super(options, utils)
+  }
+
+  async init(): Promise<void> {
+    this.nc = await connect({
+      servers: this.options.natsServerUrls,
+      name: this.options.moduleName,
+      maxPingOut: this.options.maxPingOut ?? 5,
+      // we want to receive our own events, just in case
+      noEcho: false,
+      user: this.options.user,
+      pass: this.options.password,
+      token: this.options.token,
+      headers: true,
+    })
+  }
+
+  async start() {
+    // specific route handlers
+    const routes = this.getRegisteredRoutes()
+    // wildcard ending routes
+    const prefixes = this.getRegisteredPrefixes().map(x => `${x}.>`)
+    // everyone together!
+    const patterns = prefixes.concat(routes)
+
+    this.subscriptions = patterns.map(p =>
+      this.nc.subscribe(p, {
+        callback: async (err, msg) => {
+          if (!msg) {
+            return
+          }
+
+          const message: string = msg?.data?.toString() ?? null
+
+          const route = msg.subject
+          const replyTo = msg.reply
+          const correlationId = msg.headers.get(
+            this.correlationIdHeader,
+          )
+
+          try {
+            await this.processMessage({
+              route,
+              correlationId,
+              message,
+              replyTo,
+            })
+          } catch (err) {
+            // TODO: dead letter queue
+            // await this.channel.sendToQueue(
+            //   this.deadLetterQueueName,
+            //   msg.content,
+            //   {
+            //     correlationId,
+            //     replyTo,
+            //     CC: route,
+            //   },
+            // )
+          }
+
+          // TODO: ack!
+        },
+      }),
+    )
+
+    await super.start()
+  }
+
+  async stop() {
+    // ensure all messages have been sent
+    await this.nc.flush()
+    // ensure all messages in flight have been processed
+    await this.nc.drain()
+
+    await super.stop()
+  }
+  async dispose() {
+    if (!this.nc.isClosed()) {
+      // ensure all messages have been sent
+      await this.nc.flush()
+      // ensure all messages in flight have been processed
+      await this.nc.drain()
+
+      await this.nc.close()
+    }
+
+    await super.dispose()
+  }
+
+  protected async sendMessage(
+    props: SendMessageProps,
+  ): Promise<void> {
+    const hdrs = headers()
+    hdrs.set(this.correlationIdHeader, props.correlationId)
+    if (props.isRpc) {
+      this.nc.request(props.route, Buffer.from(props.message), {
+        timeout: this.options.defaultRpcTimeout,
+        headers: hdrs,
+      })
+    } else {
+      this.nc.publish(props.route, Buffer.from(props.message), {
+        headers: hdrs,
+      })
+    }
+  }
+
+  protected async sendReplyMessage(
+    props: SendReplyMessageProps,
+  ): Promise<void> {
+    const hdrs = headers()
+    hdrs.set(this.correlationIdHeader, props.correlationId)
+
+    this.nc.publish(props.replyTo, Buffer.from(props.message), {
+      headers: hdrs,
+    })
+  }
+}
