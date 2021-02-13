38802c0d57566be1305767524879ec3ee91f5693
Kyle Bjordahl <3489222+kylebjordahl@users.noreply.github.com>
2021-02-12 17:30:31 -0800

    wip(adapters-nestjs): refactoring microservice module

--- libs/adapters/nestjs/src/lib/cheepMicroservices.module.ts
+++ libs/adapters/nestjs/src/lib/cheepMicroservices.module.ts
@@ -3,19 +3,23 @@ import {
   DynamicModule,
   OnModuleInit,
   Inject,
+  Type,
 } from '@nestjs/common'
 
 import type {
   CheepMicroservicesModuleConfig,
   CheepMicroservicesRootConfig,
 } from './types'
-import { ModuleConfigToken, RootOptionsToken } from './constants'
+import { ModuleConfigToken } from './constants'
 import { CheepApi } from './services/api.service'
 import { CheepEvents } from './services/events.service'
 import { CqrsHandlerRegistryService } from './services/cqrsHandlerRegistry.service'
 import { CheepTransportModule } from './modules/core/cheepTransport.module'
-import type { Transport, TransportBase } from '@cheep/transport'
+import type { TransportBase } from '@cheep/transport'
 import { TransportApi, transportHandler } from '@cheep/transport-api'
+import { getLeafAddresses } from './util/getLeafAddresses'
+import { Module } from '@nestjs/core/injector/module'
+import { ModuleRef } from '@nestjs/core'
 
 @Module({})
 export class CheepMicroservicesModule<
@@ -60,15 +64,27 @@ export class CheepMicroservicesModule<
       TModuleApi,
       TRemoteApi
     >,
+    private moduleRef: ModuleRef,
   ) {}
 
   onModuleInit() {
-    const handler = transportHandler<TModuleApi | TRemoteApi>(
-      this.transport,
-      this.config,
-    )
+    // const handler = transportHandler<TModuleApi | TRemoteApi>(
+    //   this.transport,
+    //   this.config,
+    // )
     // reduce handlers to array of [path, handler]
+    const leaves = getLeafAddresses(this.config.handlers)
+    for (const [path, leaf] of leaves) {
+      // check dep injection
+      const service = this.moduleRef
+        .get(leaf as Type, { strict: false })
+        .catch(() => undefined)
 
+      if (service) {
+        const methods = Object.getPrototypeOf(service)
+        this.transport.on(path.join(this.config.joinSymbol))
+      }
+    }
     // call
   }
 }
