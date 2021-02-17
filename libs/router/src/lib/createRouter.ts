da75a39c0a14a77860906862ad14d6dd21d0e3b7
Kyle Bjordahl <3489222+kylebjordahl@users.noreply.github.com>
2021-02-16 23:36:38 -0800

    feat(router): support broadcast routes

--- libs/router/src/lib/createRouter.ts
+++ libs/router/src/lib/createRouter.ts
@@ -1,42 +1,66 @@
 import {
+  MessageMetadata,
   RawHandler,
   Transport,
   TransportCompactMessage,
   TransportMessage,
 } from '@cheep/transport'
+import { TransportApi } from '@cheep/transport-api'
+import {
+  DeepPartial,
+  getLeafAddresses,
+  ReplaceLeaves,
+} from '@cheep/utils'
 
 export function createRouter<
-  TRemoteApi,
+  TLocalApi extends TransportApi,
   TRouterArgs extends Record<string, unknown>
 >(props: {
-  /** the cheep api routing key that should prefix all messages routed from the transport to the next hops */
-  routerAddress: string
+  /** unique id of this router, to avoid looping */
+  routerId: string
   transport: Transport
   nextHops: NextHop<TRouterArgs>[]
   /** RPC timeout in `ms`, if not provided will fall back to the transport default */
   rpcTimeout?: number
+  broadcastForwardPaths?: DeepPartial<
+    ReplaceLeaves<TLocalApi, boolean>
+  >
+  joinPrefix?: string
 }) {
   const outstandingRpcs = new Map<string, (...args) => unknown>()
   const {
+    routerId,
     transport,
-    routerAddress,
     nextHops,
     rpcTimeout = props.transport['options']['defaultRpcTimeout'],
   } = props
 
-  const routeRewriteRegExp = new RegExp(
-    `^${routerAddress.replace('.', '\\.')}.`,
-  )
+  const broadcastPrefixes = props.broadcastForwardPaths
+    ? getLeafAddresses(props.broadcastForwardPaths)
+        .filter(([_, include]) => include)
+        .map(([path]) => path.join(props.joinPrefix ?? '.'))
+    : []
 
   const onEveryHandler: RawHandler = (item, msg) => {
     // remove the routerAddress from the route
-    const revisedRoute = item.route.replace(routeRewriteRegExp, '')
-    const revisedItem = { ...msg, ...item, route: revisedRoute }
+    if (checkLastHop(item.metadata, routerId)) {
+      return Promise.resolve()
+    }
+
+    const revisedItem = {
+      ...msg,
+      ...item,
+      metadata: addVisitedRouterToMeta(item.metadata, routerId),
+    }
 
     // route the message to next hops
-    nextHops.forEach(h => sendNextHop(h, revisedItem))
 
-    if (msg.replyTo) {
+    const isBroadcast = broadcastPrefixes.some(p =>
+      item.route.startsWith(p),
+    )
+    nextHops.forEach(h => sendNextHop(h, isBroadcast, revisedItem))
+
+    if (msg.replyTo && !isBroadcast) {
       const rpcPromise = new Promise((resolve, reject) => {
         outstandingRpcs.set(msg.correlationId, resolve)
         setTimeout(() => {
@@ -53,17 +77,21 @@ export function createRouter<
   }
 
   Object.defineProperty(onEveryHandler, 'name', {
-    value: `CheepRouter@[${routerAddress}]`,
+    value: `CheepRouter[${routerId}]`,
     configurable: true,
   })
 
   // subscribe to anything destined for the router on the transport
-  transport.onEvery(routerAddress, onEveryHandler, true)
+  transport.onEvery('', onEveryHandler, true)
 
   nextHops.forEach(target => {
     switch (target.type) {
       case 'TUNNEL':
         target.registerReceiver(async (tunnelId, item) => {
+          // bail out if we're getting our own message
+          if (checkLastHop(item.metadata, routerId)) {
+            return
+          }
           // TODO: cover edge case where corelation ids collide across tunnels
           const rpcPromise = outstandingRpcs.get(item.correlationId)
           if (rpcPromise) {
@@ -74,13 +102,16 @@ export function createRouter<
             const props = {
               route: item.route,
               payload: item.payload,
-              metadata: {
-                ...item.metadata,
-                ...tunnelId,
-              },
+              metadata: addVisitedRouterToMeta(
+                {
+                  ...item.metadata,
+                  ...tunnelId,
+                },
+                routerId,
+              ),
               referrer: {
                 metadata: item.metadata,
-                route: routerAddress,
+                route: routerId,
               },
             }
             if (item.replyTo && item.correlationId) {
@@ -93,7 +124,10 @@ export function createRouter<
                 payload: result,
                 correlationId: item.correlationId,
                 replyTo: null,
-                metadata: item.metadata,
+                metadata: addVisitedRouterToMeta(
+                  item.metadata,
+                  routerId,
+                ),
                 route: item.route,
               })
             } else {
@@ -115,6 +149,7 @@ export interface TunnelNextHop<
   type: 'TUNNEL'
   exampleTunnelId: TTunnelId
   send: (tunnelId: TTunnelId, item: CompleteMessage) => void
+  broadcast?: (item: CompleteMessage) => void
   registerReceiver: (
     receiver: (
       tunnelId: TTunnelId,
@@ -129,10 +164,17 @@ export type NextHop<
 
 function sendNextHop<TRouterArgs extends Record<string, unknown>>(
   target: NextHop<TRouterArgs>,
+  isBroadcast: boolean,
   item: CompleteMessage,
 ) {
   switch (target.type) {
     case 'TUNNEL': {
+      // short circuit to broadcast
+      if (isBroadcast) {
+        target.broadcast(item)
+        return
+      }
+
       // pluck just the tunnelId keys from metadata
       const idKeys = Object.keys(target.exampleTunnelId)
       const tunnelId = Object.fromEntries(
@@ -143,6 +185,28 @@ function sendNextHop<TRouterArgs extends Record<string, unknown>>(
       return target.send(tunnelId, item)
     }
   }
+}
+
+function getVisitedRouters(metadata: MessageMetadata): string[] {
+  return (metadata._visitedRouters as string[]) ?? []
+}
+
+function addVisitedRouterToMeta(
+  metadata: MessageMetadata,
+  routerId: string,
+): MessageMetadata & { _visitedRouters: string[] } {
+  return {
+    ...metadata,
+    _visitedRouters: getVisitedRouters(metadata).concat([routerId]),
+  }
+}
+
+function checkLastHop(
+  metadata: MessageMetadata,
+  routerId: string,
+): boolean {
+  const routers = getVisitedRouters(metadata)
+  return routers[routers.length - 1] === routerId
 }
 
 // function receiveNextHop(
