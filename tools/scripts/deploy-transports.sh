908d3eb2daadf86c418c51ea1f4564ba4d0f0a4c
playerx <ez@jok.io>
2021-03-17 15:23:10 +0400

    chore: update package.json files with keywords and contributors information

--- tools/scripts/deploy-transports.sh
+++ tools/scripts/deploy-transports.sh
@@ -1,25 +1,28 @@
-npm run build transport-core
-# npm run build transport-aws
-npm run build transport-rabbitmq
-npm run build transport-nats
+# npm run build transport-core
+# npm run build transport-rabbitmq
+# npm run build transport-nats
 npm run build utils
 npm run build router -- --with-deps
 npm run build adapters-nestjs -- --with-deps
 
 cd dist/libs/transport/core
-npm publish --access public
+# npm publish --access public
 
 # cd ../aws
 # npm publish --access public
 
-cd ../nats
-npm publish --access public
-
-cd ../rabbitmq
-npm publish --access public
-
-# cd ../../router
+# cd ../nats
 # npm publish --access public
 
-# cd ../../adapters/nestjs
+# cd ../rabbitmq
 # npm publish --access public
+
+
+cd ../../utils
+npm publish --access public
+
+# cd ../router
+# npm publish --access public
+
+cd ../adapters/nestjs
+npm publish --access public
