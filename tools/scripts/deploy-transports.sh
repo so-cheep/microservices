9876fceb3c1e53bdc4d015d1d07c3c8ccfa93085
playerx <ez@jok.io>
2021-03-17 14:01:06 +0400

    fix(server): peer-dependencies for nats and rabbitmq libraries

--- tools/scripts/deploy-transports.sh
+++ tools/scripts/deploy-transports.sh
@@ -1,21 +1,25 @@
 npm run build transport-core
 # npm run build transport-aws
 npm run build transport-rabbitmq
+npm run build transport-nats
 npm run build utils
 npm run build router -- --with-deps
 npm run build adapters-nestjs -- --with-deps
 
 cd dist/libs/transport/core
-npm publish --access public
+# npm publish --access public
 
 # cd ../aws
-# npm publish --access public --tag beta
+# npm publish --access public
+
+cd ../nats
+npm publish --access public
 
 cd ../rabbitmq
 npm publish --access public
 
 # cd ../../router
-# npm publish --access public --tag beta
+# npm publish --access public
 
 # cd ../../adapters/nestjs
-# npm publish --access public --tag beta
+# npm publish --access public
