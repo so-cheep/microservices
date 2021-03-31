7542387a10c0145f5735658699812aecdce883e8
Ezeki <ez@jok.io>
2021-03-31 15:55:00 +0400

    chore(transport): update package versions

--- tools/scripts/deploy-transports.sh
+++ tools/scripts/deploy-transports.sh
@@ -9,14 +9,14 @@ npm run build adapters-nestjs -- --with-deps
 cd dist/libs/transport/core
 npm publish --access public
 
-# cd ../aws
-# npm publish --access public
+cd ../aws
+npm publish --access public
 
-# cd ../nats
-# npm publish --access public
+cd ../nats
+npm publish --access public
 
-# cd ../rabbitmq
-# npm publish --access public
+cd ../rabbitmq
+npm publish --access public
 
 
 # cd ../../utils
