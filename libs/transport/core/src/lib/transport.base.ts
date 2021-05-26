9c58382e9afa7a4ae0f8bb39d0deefdcdae01ed3
Kyle Bjordahl <kylebjordahl@gmail.com>
2021-05-26 12:36:43 -0700

    fix(transport): protect for non-promise handlers

--- libs/transport/core/src/lib/transport.base.ts
+++ libs/transport/core/src/lib/transport.base.ts
@@ -423,13 +423,15 @@ export abstract class TransportBase implements Transport {
       // Process additional handlers
       if (additionalHandlers.length) {
         const tasks = additionalHandlers.map((handler: any) =>
-          handler(
-            {
-              route: msg.route,
-              payload: message.payload,
-              metadata: message.metadata,
-            },
-            msg,
+          Promise.resolve(
+            handler(
+              {
+                route: msg.route,
+                payload: message.payload,
+                metadata: message.metadata,
+              },
+              msg,
+            ),
           ).catch(err => {
             if (err === WILL_NOT_HANDLE) {
               return
